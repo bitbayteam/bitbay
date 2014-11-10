86668df2e7764d8df8f7e91c513f966a6e9c6769
Bitbay <bitbaydev@gmail.com>
2014-11-10 13:59:51 +0100

    Updated end of pow time

--- src/main.h
+++ src/main.h
@@ -28,7 +28,7 @@ class CNode;
 
 class CTxMemPool;
 
-static const int LAST_POW_TIME = 1416175200;
+static const int LAST_POW_TIME = 1416402000;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
